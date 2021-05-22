ecc93f64146edd5aa6588e4d8e060ee03b4e580b
Katelinn Jones <katiejonesyo@gmail.com>
2021-05-22 15:40:14 -0700

    variables

---
+++ Variables/variables.rb
@@ -0,0 +1,4 @@
+puts "There once was a human named KT"
+puts "they were cool"
+puts "they really liked coding in ruby"
+puts "but didnt like the out put window because it doesnt automatically update in vscode"
