887d3d26f3d69c1beecd3e3e9fc9f3daa60ad22f
Katelinn Jones <katiejonesyo@gmail.com>
2021-05-22 16:02:29 -0700

    changes in variable file

--- Variables/variables.rb
+++ Variables/variables.rb
@@ -1,4 +1,6 @@
-puts "There once was a human named KT"
-puts "they were cool"
-puts "they really liked coding in ruby"
-puts "but didnt like the out put window because it doesnt automatically update in vscode"
+character_name = "Kt"
+character_age = "25"
+puts ("There once was a human named " + character_name)
+puts ("they were " +  character_age + " years old")
+puts ("they really liked the name " + character_name )
+puts "but didnt like the out put window because it doesnt automatically update in vscode"   
