a2c8fc87de382c1514df57072c46a3e7dd729286
Katelinn Jones <katiejonesyo@gmail.com>
2021-05-22 16:48:12 -0700

    math and numbers

---
+++ MathAndNumbers/math/MandN.rb
@@ -0,0 +1,37 @@
+puts 5.86543
+puts -5.86543
+puts 5 + 9
+puts 5 - 9
+puts 5 * 9
+puts 5 / 9
+puts 2**3
+puts 10 % 3
+
+num = 20
+puts num
+
+num = 20
+puts ("this method will print a string and number in the same scentence. Example, my fave num is " + num.to_s)
+
+num = 25
+puts num.abs()
+
+num = 22.678
+puts num.round()
+
+num = 20.1
+puts num.ceil()
+
+num = 20.9
+puts num.floor()
+
+puts Math.sqrt(36)
+
+puts Math.log(1)
+
+puts 1.0 + 7
+puts 1 + 7
+
+puts 10 / 7
+puts 10 / 7.0
+
