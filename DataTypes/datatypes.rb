841544c37cfb4a646cd3129ad999eb275aeebbe1
Katelinn Jones <katiejonesyo@gmail.com>
2021-05-22 16:11:14 -0700

    changes to data types file

--- DataTypes/datatypes.rb
+++ DataTypes/datatypes.rb
@@ -1,6 +1,6 @@
 variabel - name = "Kt" 
 integer - age = 25 
 floating number is decimal - gpa = 3.2
-boolean - isfemale = true
+boolean - isasian = false
 boolean - istall = true
 this has no value - flaws = nil
