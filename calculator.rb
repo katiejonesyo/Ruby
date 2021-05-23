e644bd4c34ef9f6e8e72163740e004b66bf1346a
Katelinn Jones <katiejonesyo@gmail.com>
2021-05-22 17:26:36 -0700

    calculator done

---
+++ calculator.rb
@@ -0,0 +1,14 @@
+# interactive terminal calculator
+# just run "ruby calculator.rb" in your terminal 
+
+
+puts "Enter a number: "
+num1 = gets.chomp().to_f
+puts "Enter another number: "
+num2 = gets.chomp().to_f
+
+puts (num1+ num2)
+
+
+# .to_i = to turn into an integer
+# .to_f = to turn into a floating number 
