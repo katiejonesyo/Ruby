edb402903d61633514077d7d6d0e128c5a698d15
Katelinn Jones <katiejonesyo@gmail.com>
2021-05-22 23:19:04 -0700

    while loops

---
+++ WhileLoops/whileloops.rb
@@ -0,0 +1,13 @@
+# The while statement is simple, it executes code repeatedly as long as the condition is true. 
+# A while loop's conditional is separated from code by the reserved word 'do', a newline, backslash \, or a semicolon.
+# specify a condition
+
+
+
+index = 1
+while index <= 8
+    puts index
+    index += 1
+
+end
+
