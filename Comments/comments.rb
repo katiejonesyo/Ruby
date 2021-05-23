64150bedfae3341d1e0ea7749cf5de5f1610da97
Katelinn Jones <katiejonesyo@gmail.com>
2021-05-23 00:03:55 -0700

    comments

---
+++ Comments/comments.rb
@@ -0,0 +1,8 @@
+# use "#" to comment out a code or shift + / when highlighting a line of code.
+# you can also put comments after a line of code.
+
+puts "comments are fun!" #this is also commented out
+
+# this line is commented out
+
+
